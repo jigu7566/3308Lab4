b42fee6b75d6c9991e20d62f4f18d120434b00d7
jigu7566 <jing.guo@colorado.edu>
2014-09-17 17:35:37 -0600

    helloworld.java.added

---
+++ src/hello/java/Hellojava.java
@@ -0,0 +1,11 @@
+package hello.java;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); //Display the string
+		// TODO Auto-generated method stub
+
+	}
+
+}
