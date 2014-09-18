44dc93a122bc8480b918bb6cf9ce66dc62951500
jigu7566 <jing.guo@colorado.edu>
2014-09-17 18:11:56 -0600

    ll

--- src/hello/java/Hellojava.java
+++ src/hello/java/Hellojava.java
@@ -5,7 +5,7 @@ public class Hellojava {
 	public static void main(String[] args) {
 		System.out.println("Hello World"); //Display the string
 		// TODO Auto-generated method stub
-
+		System.out.println("How are you?");//
 	}
 
 }
